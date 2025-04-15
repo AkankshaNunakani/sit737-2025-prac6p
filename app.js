58b7551e524cb92a1465c58a1c8600d2b84ff5be
Akanksha Nunakani <akanksha.nunakani5@gmail.com>
2025-04-15 18:29:33 +1000

    Initial commit for Kubernetes task

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Node.js app running in Kubernetes!');
+  res.send(" Updated Kubernetes Node.js App!");
 });
 
 app.listen(port, () => {
